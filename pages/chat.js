a51ffd790cc10e1bbdb8b23f711d455e3d301260
Ganesh <mekalaganeshreddy796@gmail.com>
2024-06-03 07:48:53 +0530

    message-proble-solved-by=updating-useeffect

--- pages/chat.js
+++ pages/chat.js
@@ -3,13 +3,14 @@
 import React, { useState, useEffect } from 'react';
 import style from './chat.module.css'
 import { useRouter } from 'next/router';
-import Link from 'next/link';
+
 import InfiniteScroller from 'react-infinite-scroller';
 import dayjs from 'dayjs';
+import Chatside from './chatside';
 
 
 function Chat() {
-  const [questions, setQuestions] = useState([]);
+  
 
   const [val, setval] = useState([]);
   const [lent, setlent] = useState();
@@ -42,7 +43,7 @@ function Chat() {
 
 
   const handleSubmit = async (e) => {
-
+if(user.Name){
     const today = dayjs().format('dddd'); // 'dddd' gives the full name of the day
 
     const currentDate = new Date();
@@ -103,11 +104,13 @@ function Chat() {
       console.error('Error:', error);
       alert('Error occurred while storing data.');
     }
+  }
   };
 
 
   useEffect(() => {
-     setInterval(async () => {
+    
+     setInterval(async() => {
     
 
       try {
@@ -152,12 +155,12 @@ function Chat() {
 
       }
 
-
-
     }, 7000)
+  
+  
 
 
-  }, []);
+  }, [you]);
   const currentDate = new Date();
   console.log("present date ", currentDate)
   useEffect(() => {
@@ -235,31 +238,7 @@ function Chat() {
   } else {
     something = message
   }
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch(`https://acehack-65f02-default-rtdb.firebaseio.com/${me}/Statuses/.json`);
-        if (res.ok) {
-          const data = await res.json();
-          console.log("Fetched data:", data);
-
-          const questionsArray = Object.keys(data).map(key => ({
-            Id: key,
-            ...data[key].updatedQuestions[0]
-          }));
-          setQuestions(questionsArray);
-          console.log('Questions array:', questionsArray);
-        } else {
-          throw new Error('Failed to fetch data.');
-        }
-      } catch (error) {
-        console.error('Error:', error);
-      }
-
-
-    };
-    fetchData();
-  }, []);
+ 
 
   return (
     <>
@@ -268,18 +247,7 @@ function Chat() {
       </div>
 
       <div id={style.chat}>
-        <div id={style.names}>
-          {questions.map((question, index) => (
-           <Link href={`/chat?me=${me}&you=${question.Username}`} key={index}>
-           
-            <div id={style.details}>
-              <img src='doughtprofile.png' alt='profile' />
-              <h3>{question.Username}</h3>
-            </div>
-            </Link>
-
-          ))}
-        </div>
+       <Chatside/>
         <div id={style.line}></div>
 
         <input id={style.messagein}
